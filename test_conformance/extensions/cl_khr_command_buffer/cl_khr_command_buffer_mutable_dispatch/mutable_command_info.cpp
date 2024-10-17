db1cd24c21b275817b643d5514892551e0a22c4e
Michal Babej <michal.babej@intel.com>
2024-10-17 18:16:29 +0300

    cl_khr_command_buffer_mutable_dispatch: fix incorrect logic in Skip check

--- test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/mutable_command_info.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/cl_khr_command_buffer_mutable_dispatch/mutable_command_info.cpp
@@ -119,6 +119,8 @@ struct PropertiesArray : public InfoMutableCommandBufferTest
     virtual bool Skip() override
     {
         Version device_version = get_device_cl_version(device);
+        if (InfoMutableCommandBufferTest::Skip())
+            return true;
         if ((device_version >= Version(3, 0))
             || is_extension_available(device, "cl_khr_extended_versioning"))
         {
@@ -133,7 +135,7 @@ struct PropertiesArray : public InfoMutableCommandBufferTest
                 return true;
             }
         }
-        return InfoMutableCommandBufferTest::Skip();
+        return false;
     }
 
     cl_int Run() override
