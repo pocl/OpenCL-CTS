bf7e9a9198043ed0dc69dcad6eacdf4cae59e3b2
Michal Babej <90404+franz@users.noreply.github.com>
2024-10-27 01:34:58 +0300

    cl_khr_command_buffer_mutable_dispatch & negative_command_buffer tests: fix Skip logic

--- test_conformance/extensions/cl_khr_command_buffer/negative_command_buffer_fill.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/negative_command_buffer_fill.cpp
@@ -417,6 +417,14 @@ struct CommandBufferCommandFillBufferMutableHandleNotNull
 {
     using CommandFillBaseTest::CommandFillBaseTest;
 
+  bool Skip() override
+  {
+    if (CommandFillBaseTest::Skip())
+        return true;
+    return is_extension_available(device,
+                                  "cl_khr_command_buffer_mutable_dispatch");
+  }
+
     cl_int Run() override
     {
         cl_mutable_command_khr mutable_handle;
@@ -440,6 +448,14 @@ struct CommandBufferCommandFillImageMutableHandleNotNull
     : public CommandFillBaseTest<true>
 {
     using CommandFillBaseTest::CommandFillBaseTest;
+
+  bool Skip() override
+  {
+    if (CommandFillBaseTest::Skip())
+      return true;
+    return is_extension_available(device,
+                                  "cl_khr_command_buffer_mutable_dispatch");
+  }
 
     cl_int Run() override
     {
