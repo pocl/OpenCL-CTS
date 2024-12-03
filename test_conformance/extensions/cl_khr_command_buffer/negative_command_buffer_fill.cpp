77a4ec0f6783226e5e6bd4fe00b2d93d672053e1
Michal Babej <michal.babej@intel.com>
2024-12-03 13:38:17 +0200

    partially revert commit ac736ac1aa970d67ad5454fda30a863e4021d369
    Reverts part of "cl_khr_command_buffer_mutable_dispatch & negative_command_buffer tests: fix Skip logic"
    commit. This was found by review to be invalid.

--- test_conformance/extensions/cl_khr_command_buffer/negative_command_buffer_fill.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/negative_command_buffer_fill.cpp
@@ -417,14 +417,6 @@ struct CommandBufferCommandFillBufferMutableHandleNotNull
 {
     using CommandFillBaseTest::CommandFillBaseTest;
 
-  bool Skip() override
-  {
-    if (CommandFillBaseTest::Skip())
-        return true;
-    return is_extension_available(device,
-                                  "cl_khr_command_buffer_mutable_dispatch");
-  }
-
     cl_int Run() override
     {
         cl_mutable_command_khr mutable_handle;
@@ -448,14 +440,6 @@ struct CommandBufferCommandFillImageMutableHandleNotNull
     : public CommandFillBaseTest<true>
 {
     using CommandFillBaseTest::CommandFillBaseTest;
-
-  bool Skip() override
-  {
-    if (CommandFillBaseTest::Skip())
-      return true;
-    return is_extension_available(device,
-                                  "cl_khr_command_buffer_mutable_dispatch");
-  }
 
     cl_int Run() override
     {
