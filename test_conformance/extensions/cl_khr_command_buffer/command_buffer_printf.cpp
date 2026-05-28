9f7fbb39cdd38f58844f4ef06cb9fcea8e0772e9
Michal Babej <michal.babej@tuni.fi>
2026-05-28 16:36:26 +0300

    workaround for GCC warning (-Werror)

--- test_conformance/extensions/cl_khr_command_buffer/command_buffer_printf.cpp
+++ test_conformance/extensions/cl_khr_command_buffer/command_buffer_printf.cpp
@@ -283,7 +283,9 @@ struct CommandBufferPrintfTest : public BasicCommandBufferTest
                 std::max(min_pattern_length, rand() % max_pattern_length);
 
             std::vector<cl_char> pattern(pattern_length + 1, pattern_character);
-            pattern.back() = '\0';
+            // workaround for GCC warning
+            if (pattern.size() > 0)
+              pattern.back() = '\0';
             enqueue_passes[i] = {
                 pattern,
                 { cl_int(i * offset), cl_int(pattern_length) },
